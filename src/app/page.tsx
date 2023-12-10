3a50b99b37dd92734bf23013b7bac49dab3ea10c
DanielCononie <danielcononie1278@gmail.com>
2023-12-10 18:34:03 -0500

    More functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,94 @@ import React, { ChangeEventHandler, useState } from 'react';
 export default function Home() {
 
   interface TODO {
+    id: string,
     description: string,
     time: Date,
   }
 
-  const [todo, setTodo] = useState<TODO[]>([]);
+  const [todos, setTodos] = useState<TODO[]>([]);
+  const [todo, setTodo] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [edit, setEdit] = useState<boolean>(false);
+  
+  const addTodo = () => {
+    if(todo.length > 0) {
 
+      const randomNum : string = ((Math.random() * 10000) / (Math.random() * 10)).toFixed(0).toString()
+      setTodos([...todos, {
+        id: randomNum,
+        description: todo,
+        time: new Date(),
+      }])
+    } else {
+      setError("Please enter a todo first");
+    }
+    
+  }
 
   return (
     <main className='p-5'>
+      <div className='flex flex-col justify-center text-center'>
+        <h2 className='text-center'>Add a todo</h2>
+        <br />
+        <span className='flex justify-center'>
+        <input type="text" className='border-2 rounded-lg' 
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) =>  {
+            if(e.key === "Enter") {
+              addTodo();
+            }
+        }} 
+        onChange ={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setTodo(e.target.value);
+          }}/>
+          <button className='border-2 hover:bg-slate-200 rounded-lg font-bold text-lg pl-3 pr-3 ml-3' onClick={addTodo}>+</button>
+        </span>
+        <span style={{color: 'red', fontWeight: 'bold'}}>{error}</span>
+      </div>
       <div>
-        <label htmlFor="">Add a todo</label>
-        <input type="text" className='border-2' onChange ={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setTodo([...todo, {
-              description: e.target.value,
-              time: new Date(),
-            }])
-        }}/>
-        <button>Add todo</button>
+        {todos.map((task, index) => (
+          <div key={task.id} className='border-2 w-[70%] mt-2 rounded-lg '>
+            <p><small>{task.time.toLocaleDateString()}</small></p>
+            <span className='flex'>
+              {edit ? 
+                (
+                 <>
+                   <input className='border-2 rounded-lg h-8 mr-2 ml-2' placeholder={`${task.description}...`}/>
+                   <button 
+                   className='mr-5 mb-3 border-2 hover:bg-slate-200 rounded-lg font-bold text-lg pl-3 pr-3 '
+                   >
+                    Save
+                    </button>
+                   <button  
+                   className='mb-3 border-2 hover:bg-slate-200 rounded-lg font-bold text-lg pl-3 pr-3'
+                   onClick={() => setEdit(false)}>
+                    Cancel
+                    </button>
+                 </>
+                )
+               :
+               (
+                <>
+                  <p className='mr-20 w-[40%] mw-[40%] overflow-x-auto whitespace-pre-wrap ml-2'>{task.description}</p>
+                  <button 
+                   className='mr-5 mb-3 border-2 hover:bg-slate-200 rounded-lg font-bold text-lg pl-3 pr-3 '
+                   onClick={() => setEdit(!edit)}>
+                    Edit
+                  </button>
+                  <button 
+                  className='mb-3 border-2 hover:bg-slate-200 rounded-lg font-bold text-lg pl-3 pr-3'
+                  onClick={() => {
+                    setTodos(todos.filter((element) => element.id !== task.id))
+                  }}>
+                     Done
+                 </button>
+                </>
+               )
+              } 
+             
+            </span>
+          </div>
+        ))}
       </div>
     </main>
   )
